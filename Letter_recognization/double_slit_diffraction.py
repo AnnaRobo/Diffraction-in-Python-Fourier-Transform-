6adce7a8dbff608fe1ad1f250e89273439e669cc
Anushka Tyagi <anushka_t@me.iitr.ac.in>
2022-01-30 14:43:18 +0530

    PaAC ,IIT Roorkee

---
+++ Letter_recognization/double_slit_diffraction.py
@@ -0,0 +1,27 @@
+import cv2
+import numpy as np
+#import scipy as sp
+import matplotlib.pyplot as plt
+plt.style.use(['seaborn-notebook'])
+#import sympy as smp
+#from skimage import color
+#from skimage import io
+#from scipy.fft import fftfreq
+#from scipy.fft import fft, ifft, fft2, ifft2
+
+img = cv2.imread('D:/PaAC Project/Final_Report/Letter_recognization/double_slit_paint.png', cv2.IMREAD_GRAYSCALE)
+
+#img = color.rgb2gray(io.imread('D:/PaAC Project/Final_Report/Red_E.jpg'))
+f = np.fft.fft2(img)
+fshift = np.fft.fftshift(f)
+
+#magnitude_spectrum = 20 * np.log(np.abs(fshift))
+magnitude_spectrum = 20*(np.log(np.abs(fshift)))
+magnitude_spectrum = np.asarray(magnitude_spectrum, dtype=np.uint8)
+img_and_magnitude = np.concatenate((img, magnitude_spectrum), axis=1)
+
+cv2.imwrite('double_slit_diffraction ' + 'image.png', img_and_magnitude)
+cv2.imshow('Double_slit_Diffraction', img_and_magnitude)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
